fd1ad6f249cacc72aeabf6228bfc90f15505e20a
kozr <zxzflow@gmail.com>
2023-02-25 19:01:27 -0800

    wallet

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import supabase from '@/utilities/supabase'
+import Wallet from '@/components/Wallet'
 
 const signInWithGoogle = async () => {
   const { data, error } = await supabase.auth.signInWithOAuth({
@@ -8,10 +9,11 @@ const signInWithGoogle = async () => {
 
 export default function Home() {
   return (
-    <>
+    <div className='px-10'>
       <button onClick={() => signInWithGoogle()} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
         Login
       </button>
-    </>
+      <Wallet balance={1000.10} />
+    </div>
   )
 }
