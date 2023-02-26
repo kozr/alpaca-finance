2ba1e6517c7307d47a7b0945a69a4d699257ae5b
ylkwan <ylkwan@students.cs.ubc.ca>
2023-02-25 16:46:01 -0800

    added tailwind

---
+++ src/pages/feed.js
@@ -0,0 +1,9 @@
+export default function Feed() {
+    return (
+        <>
+            <div className="font-bold text-3xl">
+                FEED
+            </div>
+        </>
+    )
+}
--- src/utilities/supabase.js
+++ src/utilities/supabase.js
@@ -1,6 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Create a single supabase client for interacting with your database
-const supabase = createClient(process.env['SUPABASE_PROJECT_URL'], process.env['SUPABASE_API_KEY'])
+const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_PROJECT_URL, 
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+)
 
 export default supabase
