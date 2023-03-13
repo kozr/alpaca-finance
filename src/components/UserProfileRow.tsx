4112f6eeb511b99df0ab7e4fed68e99c138aeb2b
kozr <zxzflow@gmail.com>
2023-03-12 17:46:13 -0700

    fix types export

--- src/components/UserProfileRow.tsx
+++ src/components/UserProfileRow.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { User } from '../types'
+import Image from 'next/image'
 
-const UserProfileRow = ({ user }) => {
+type UserProfileRowProps = {
+  user: User
+}
+
+const UserProfileRow = ({ user }: UserProfileRowProps) => {
   return (
     <div className="flex flex-col items-center justify-center">
       <div className="flex items-center justify-center w-32 h-32 rounded-full bg-gray-100">
-        <img
+        <Image
           className="object-cover w-32 h-32 rounded-full"
           src={user.avatar_url}
           alt={`${user.first_name}'s avatar`}
@@ -12,7 +18,7 @@ const UserProfileRow = ({ user }) => {
       </div>
       <div className="mt-4">
         <h2 className="text-2xl font-semibold text-gray-800">
-          {user.name}
+          {user.first_name} {user.last_name}
         </h2>
         <p className="text-sm font-medium text-gray-600">
           {user.email}
@@ -21,3 +27,5 @@ const UserProfileRow = ({ user }) => {
     </div>
   );
 }
+
+export default UserProfileRow;
---
+++ src/types/index.ts
@@ -0,0 +1,9 @@
+import Payment from "./schema/Payment";
+import Transaction from "./schema/Transaction";
+import User from "./schema/User";
+
+export type {
+  Payment,
+  Transaction,
+  User,
+};
