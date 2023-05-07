f17231d305d4df137bbd79f34fe13070018553ca
kozr <zxzflow@gmail.com>
2023-05-06 20:41:57 -0700

    set up endpoint for viewing related payments

---
+++ src/pages/api/payments/index.js
@@ -0,0 +1,12 @@
+import get from './get'
+
+export default async function handler(req, res) {
+  switch (req.method) {
+    case "GET":
+      await get(req, res);
+      break;
+    default:
+      res.setHeader('Allow', ['GET', 'POST', 'PUT', 'DELETE'])
+      res.status(405).end(`Method ${req.method} Not Allowed`)
+  }
+}
