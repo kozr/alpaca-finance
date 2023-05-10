5cb6fc3395e4daf3aba092488dab3294453ca589
kozr <zxzflow@gmail.com>
2023-05-10 02:45:56 -0700

    enable tl

--- src/utilities/redis/index.ts
+++ src/utilities/redis/index.ts
@@ -7,6 +7,7 @@ export const redisOptions = {
   host: redisUrl.hostname,
   port: Number(redisUrl.port),
   password: redisUrl.auth?.split(":")[1],
+  tls: {},
 };
 
 export const redisConnection = new Redis(redisOptions);
