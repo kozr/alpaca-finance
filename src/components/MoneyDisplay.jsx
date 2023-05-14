afc0d5797937c0abeedd6f8fd9c5312d17477c15
kozr <zxzflow@gmail.com>
2023-05-14 02:01:11 -0700

    fix neg balance

--- src/components/MoneyDisplay.jsx
+++ src/components/MoneyDisplay.jsx
@@ -3,16 +3,18 @@ import React, { useEffect, useState } from "react";
 const MoneyDisplay = ({ total }) => {
   const [dollars, setDollars] = useState(0);
   const [cents, setCents] = useState(0);
+  const [prefix, setPrefix] = useState("");
 
   useEffect(() => {
-    setDollars(Math.trunc(total));
+    setDollars(Math.abs(Math.trunc(total)));
     setCents(Math.trunc(Math.abs((total * 100) % 100)));
+    setPrefix(total < 0 ? "-" : "")
   }, [total]);
 
   return (
     <>
       <h2 className="text-4xl pt-3">
-        ${dollars.toLocaleString("en-US")}.{cents}
+        {prefix}${dollars.toLocaleString("en-US")}.{cents}
       </h2>
     </>
   );
