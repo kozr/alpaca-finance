1309e950475f570b18743b9395b15d248fb9ed55
kozr <zxzflow@gmail.com>
2023-06-27 21:02:25 -0700

    update deposit

--- src/pages/api/reconcile/index.js
+++ src/pages/api/reconcile/index.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
   const { data, _error } = await supabase.from("user").select("*");
 
   const deposits = {
-    AndyMa: 4800,
+    AndyMa: 4950,
     LeonLin: -8850,
     HenryShang: 2966,
     NicholasWong: 2250,
