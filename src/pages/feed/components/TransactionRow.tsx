fac5d9d1c52eade583dfc30546b7d421e981b122
Henry <skippy24680@gmail.com>
2023-07-06 17:54:00 -0700

    Removed "pending" before total amount in transactionRow

--- src/pages/feed/components/TransactionRow.tsx
+++ src/pages/feed/components/TransactionRow.tsx
@@ -37,7 +37,7 @@ const TransactionRow = ({ transactionDetails, onClick }: TransactionRowProps) =>
       </div>
       <div className="flex flex-col">
         <div className="text-sm font-light text-gray-600">
-          {state}: {total_amount}
+          {total_amount}
         </div>
       </div>
     </div>
