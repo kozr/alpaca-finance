69c47dcb88d92a8cbe93425669361b30674456a4
Henry <skippy24680@gmail.com>
2023-07-08 18:17:05 -0700

    Fixed improper display of decimal input in MoneyInput
    Changed MoneyInput to display a handled string instead of an absolute number.
    
    Allows users to see decimal point being added after clicking decimal button.
    
    Also fixes issue with decimals not displaying properly with input.

--- src/pages/request/components/MoneyInput.tsx
+++ src/pages/request/components/MoneyInput.tsx
@@ -1,8 +1,19 @@
 import React, { useEffect, useState } from "react";
 
+const setDisplay = (value, inputValue) => {
+  // regex match cases: . | .0 | .00, prepends 0 in front of decimal
+  const regex = /^\./;
+  var display = value.toString();
+  if(regex.test(display)) {
+    display = "0" + display;
+  }
+  return display;
+}
+
 const MoneyInput = ({ value, ...props }) => {
   const [inputValue, setInputValue] = useState(value || "");
   const [prefix, setPrefix] = useState("$");
+  const display = setDisplay(value, inputValue);
 
   useEffect(() => {
     if (value < 0) {
@@ -14,8 +25,8 @@ const MoneyInput = ({ value, ...props }) => {
   return (
     <div className="flex flex-row justify-center text-5xl items-center">
       <span className="pb-3 border-b-2 border-money-grey min-w-[150px] text-center">
-        &nbsp;{inputValue && "$"}
-        {inputValue}&nbsp;
+        &nbsp;{display && "$"}
+        {display}&nbsp;
       </span>
     </div>
   );
