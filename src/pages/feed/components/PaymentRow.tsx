71dd1973786205f29acb56fef63f036762ab8bd1
Scorpsy <matthewraynard65@gmail.com>
2023-07-29 14:10:02 -0700

    added + icon to switch between Accept and Decline button

--- src/pages/feed/components/PaymentRow.tsx
+++ src/pages/feed/components/PaymentRow.tsx
@@ -15,6 +15,7 @@ const PaymentRow = ({ paymentDetails, onClick }: PaymentRowProps) => {
   const currentUser = authContext.user;
 
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isAccept, setIsAccept] = useState(true);
 
   const {
     id,
@@ -73,110 +74,86 @@ const PaymentRow = ({ paymentDetails, onClick }: PaymentRowProps) => {
       }
    };
 
-  return (
-    <div
-      className="flex flex-row items-center justify-between pt-5"
-      onClick={onClick}
-    >
-      <div className="flex items-center">
-        <div className="flex-none w-16 h-16 bg-gray-100">
-          <Image
-            className="object-cover w-16 h-16 rounded-md"
-            src={targetAvatarUrl}
-            alt={`${targetName}'s avatar`}
-            width={128}
-            height={128}
-          />
-        </div>
-        <div className="pl-3 flex flex-col">
-          <div className="font-medium">{targetName}</div>
-          {reason ? (
-            <div className="flex items-start text-sm font-light text-gray-600">
-              <div>{reason}</div>
-              <div>&nbsp;</div>
-              <div>({state})</div>
+    return (
+        <div
+            className="flex flex-row items-center justify-between pt-5"
+            onClick={onClick}
+        >
+            <div className="flex items-center">
+                <div className="flex-none w-16 h-16 bg-gray-100">
+                    <Image
+                        className="object-cover w-16 h-16 rounded-md"
+                        src={targetAvatarUrl}
+                        alt={`${targetName}'s avatar`}
+                        width={128}
+                        height={128}
+                    />
+                </div>
+                <div className="pl-3 flex flex-col">
+                    <div className="font-medium">{targetName}</div>
+                    {reason ? (
+                        <div className="flex items-start text-sm font-light text-gray-600">
+                            <div>{reason}</div>
+                            <div>&nbsp;</div>
+                            <div>({state})</div>
+                        </div>
+                    ) : (
+                        <div className="text-sm font-light text-gray-600">({state})</div>
+                    )}
+                </div>
             </div>
-          ) : (
-            <div className="text-sm font-light text-gray-600">({state})</div>
-          )}
+            <div className="flex flex-row items-center justify-end">
+                {!userIsPayee && state === "pending" && (
+                    <>
+                        <div className="ml-2">
+                            <button onClick={() => setIsAccept(!isAccept)}>
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
+                                </svg>
+                            </button>
+                        </div>
+
+                        <div className="ml-4">
+                            <Button
+                                size="small"
+                                backgroundColor={isAccept ? "bg-positive-green" : "bg-negative-red"}
+                                onClick={isAccept ? onClickAccept : onClickDecline}
+                            >
+                                {isLoading ? (
+                                    <svg
+                                        className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                    >
+                                        <circle
+                                            className="opacity-25"
+                                            cx="12"
+                                            cy="12"
+                                            r="10"
+                                            stroke="currentColor"
+                                            strokeWidth="4"
+                                        ></circle>
+                                        <path
+                                            className="opacity-75"
+                                            fill="currentColor"
+                                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                                        ></path>
+                                    </svg>
+                                ) : (
+                                    isAccept ? "Accept" : "Decline"
+                                )}
+                            </Button>
+                        </div>
+                        
+                    </>
+                )}
+                <div className="ml-4 text-sm font-light text-gray-600">
+                    {userIsPayee ? amount : -amount}
+                </div>
+            </div>
         </div>
-      </div>
-          <div className="flex flex-row items-center justify-end">
-              {!userIsPayee && state === "pending" && (
-                  <>
-                      <div className="ml-4">
-                          <Button
-                              size="small"
-                              backgroundColor="bg-negative-red"
-                              onClick={onClickDecline}
-                          >
-                              {isLoading ? (
-                                  <svg
-                                      className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-                                      xmlns="http://www.w3.org/2000/svg"
-                                      fill="none"
-                                      viewBox="0 0 24 24"
-                                  >
-                                      <circle
-                                          className="opacity-25"
-                                          cx="12"
-                                          cy="12"
-                                          r="10"
-                                          stroke="currentColor"
-                                          strokeWidth="4"
-                                      ></circle>
-                                      <path
-                                          className="opacity-75"
-                                          fill="currentColor"
-                                          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                                      ></path>
-                                  </svg>
-                              ) : (
-                                  "Decline"
-                              )}
-                          </Button>
-                      </div>
-                      <div className="ml-4">
-                          <Button
-                              size="small"
-                              backgroundColor="bg-positive-green"
-                              onClick={onClickAccept}
-                          >
-                              {isLoading ? (
-                                  <svg
-                                      className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-                                      xmlns="http://www.w3.org/2000/svg"
-                                      fill="none"
-                                      viewBox="0 0 24 24"
-                                  >
-                                      <circle
-                                          className="opacity-25"
-                                          cx="12"
-                                          cy="12"
-                                          r="10"
-                                          stroke="currentColor"
-                                          strokeWidth="4"
-                                      ></circle>
-                                      <path
-                                          className="opacity-75"
-                                          fill="currentColor"
-                                          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                                      ></path>
-                                  </svg>
-                              ) : (
-                                  "Accept"
-                              )}
-                          </Button>
-                      </div>
-                      
-                  </>
-              )}
-              <div className="ml-4 text-sm font-light text-gray-600">
-                  {userIsPayee ? amount : -amount}
-              </div>
-          </div>
-    </div>
-  );
+    );
 };
 
 export default PaymentRow;
