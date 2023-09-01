aa52caae428973759b7d64b07a9d39fd61ffe110
kozr <zxzflow@gmail.com>
2023-09-01 00:24:58 -0700

    no tls

--- src/utilities/redis/index.ts
+++ src/utilities/redis/index.ts
@@ -10,7 +10,7 @@ export const redisOptions = {
 };
 
 // if (process.env.NODE_ENV !== 'development') {
-redisOptions['tls'] = {};
+// redisOptions['tls'] = {};
 // }
 
 export const redisConnection = new Redis(redisOptions);
