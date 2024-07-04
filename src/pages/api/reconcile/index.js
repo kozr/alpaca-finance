7860e020862ab678387dedd7415e20c58ae37bbd
Nicholas Wong <40312631+kozr@users.noreply.github.com>
2024-07-04 11:58:04 -0700

    Pclip 5/16 -$400

--- src/pages/api/reconcile/index.js
+++ src/pages/api/reconcile/index.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
     AdrianLam: -6000,
     VivianXu: 400,
     AndyKwan: 650,
-    PhillipLiu: 2900,
+    PhillipLiu: 2500,
     Charlesnull: 67
   };
 
